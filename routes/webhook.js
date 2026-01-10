d8c7bba51635f213b3da8feb1536d324f858b44b
tainuth1 <tainuth1@gmail.com>
2026-01-10 13:21:18 +0700

    Test bot

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const router = express.Router();
 const { verifyGitHubSignature } = require("../middleware/githubAuth");
-const { handleGitHubWebhook } = require("../services/githubWebhookService");
+const { handleGitHubWebhook } = require("../services/githubWebhookService"); 
 
 /**
  * POST /github/webhook
